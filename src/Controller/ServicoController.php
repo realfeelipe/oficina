90608ee6dab5cb4322a09bf21618fd2ff1d62ee5
realfeelipe <realfeelipe@gmail.com>
2022-07-03 09:08:39 -0300

    url amigável

--- src/Controller/ServicoController.php
+++ src/Controller/ServicoController.php
@@ -55,7 +55,7 @@ final class ServicoController
 
         $campos = array(
             'titulo' => $titulo,
-            'url_amigavel' => '',
+            'url_amigavel' => $this->gerarUrlAmigavel($titulo),
             'descricao' => $descricao,
             'imagem_principal' => '',
             'data_cadastro' => $data,
@@ -73,5 +73,20 @@ final class ServicoController
         );
         $renderer = new PhpRenderer(DIRETORIO_TEMPLATES_ADMIN."/servico");
         return $renderer->render($response, "servicos.php", $data);
+    }
+    private function gerarUrlAmigavel($url) {
+
+        $search = ['@<script[^>]*?>.*?</script>@si', '@<style[^>]*?>.*?</style>@siU', '@<[\/\!]*?[^<>]*?>@si', '@<![\s\S]*?--[ \t\n\r]*>@'];
+    
+        $string = preg_replace($search, '', $url);
+    
+        $table = ['Š'=>'S','š'=>'s','Đ'=>'Dj','đ'=>'dj','Ž'=>'Z','ž'=>'z','Č'=>'C','č'=>'c','Ć'=>'C','ć'=>'c','À'=>'A','Á'=>'A','Â'=>'A','Ã'=>'A','Ä'=>'A','Å'=>'A','Æ'=>'A','Ç'=>'C','È'=>'E','É'=>'E','Ê'=>'E','Ë'=>'E','Ì'=>'I','Í'=>'I','Î'=>'I','Ï'=>'I','Ñ'=>'N','Ò'=>'O','Ó'=>'O','Ô'=>'O','Õ'=>'O','Ö'=>'O','Ø'=>'O','Ù'=>'U','Ú'=>'U','Û'=>'U','Ü'=>'U','Ý'=>'Y','Þ'=>'B','ß'=>'Ss','à'=>'a','á'=>'a','â'=>'a','ã'=>'a','ä'=>'a','å'=>'a','æ'=>'a','ç'=>'c','è'=>'e','é'=>'e','ê'=>'e','ë'=>'e','ì'=>'i','í'=>'i','î'=>'i','ï'=>'i','ð'=>'o','ñ'=>'n','ò'=>'o','ó'=>'o','ô'=>'o','õ'=>'o','ö'=>'o','ø'=>'o','ù'=>'u','ú'=>'u','û'=>'u','ý'=>'y','ý'=>'y','þ'=>'b','ÿ'=>'y','Ŕ'=>'R','ŕ'=>'r'
+        ];
+    
+        $string = strtr($string, $table);
+        $string = mb_strtolower($string);
+        $string = preg_replace("/[^a-z0-9_\s-]/", "", $string);
+        $string = str_replace(" ", "-", $string);
+        return $string;
     }
 }
