74037ce459388f407fb35c2d1914c5aa6ce20731
realfeelipe <realfeelipe@gmail.com>
2022-07-03 19:45:42 -0300

    Update servicos.php

--- src/Views/admin/servico/servicos.php
+++ src/Views/admin/servico/servicos.php
@@ -45,11 +45,11 @@
                 </tbody>
             </table>
             <div class="paginacao">
-                <?php if(isset($data['informacoes']['paginaAnterior'])){ ?>
+                <?php if(isset($data['informacoes']['paginaAnterior']) && $data['informacoes']['paginaAnterior'] !== false){ ?>
                 <a href="<?=$data['informacoes']['paginaAnterior']?>"><i class="fas fa-arrow-circle-left"></i></a>
                 <?php } ?>
                 <span><?=$data['informacoes']['paginaAtual']?></span>
-                <?php if(isset($data['informacoes']['proximaPagina'])){ ?>
+                <?php if(isset($data['informacoes']['proximaPagina']) && $data['informacoes']['proximaPagina'] !== false){ ?>
                 <a href="<?=$data['informacoes']['proximaPagina']?>"><i class="fas fa-arrow-circle-right"></i></a>
                 <?php } ?>
             </div>
